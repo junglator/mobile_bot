8c71321f711e049eee55f6288ee4842b14b7e45c
junglator <srivastavaadi17@gmail.com>
2023-05-10 21:48:46 +0530

    minor changes

--- urdf/install/setup.ps1
+++ urdf/install/setup.ps1
@@ -23,6 +23,7 @@ function _colcon_prefix_chain_powershell_source_script {
 
 # source chained prefixes
 _colcon_prefix_chain_powershell_source_script "/opt/ros/humble\local_setup.ps1"
+_colcon_prefix_chain_powershell_source_script "/home/junglator/learn_ws/install\local_setup.ps1"
 _colcon_prefix_chain_powershell_source_script "/home/junglator/install\local_setup.ps1"
 _colcon_prefix_chain_powershell_source_script "/home/junglator/pandora_ws/install\local_setup.ps1"
 
